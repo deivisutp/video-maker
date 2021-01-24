501f2d38546933432f02148fe270a484841ef4fa
Deivis Utpadel <deivis.utpadel@philips.com>
2021-01-24 13:38:09 -0300

    feature: add google trends

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const readline = require('readline-sync')
+const Parser = require('rss-parser')
 
-function start() {
+const TREND_URL = 'https://trends.google.com/trends/trendingsearches/daily/rss?geo=BR' 
+
+async function start() {
     const content = {}
 
-    content.searchTerm = askAndReturnSearchTerm()
+    content.searchTerm = await askAndReturnSearchTerm()
     content.prefix = askAndReturnPrefix()
 
-    function askAndReturnSearchTerm() {
-        return readline.question('Type a Wikipidea search term: ')
+    async function askAndReturnSearchTerm() {
+        response = readline.question('Type a Wikipidea search term or G to fecth google trends: ')
+
+        return (response.toUpperCase() === 'G') ? await askAndReturnTrend() : response
+    }
+
+    async function askAndReturnTrend() {
+        console.log('Plesase wait....')
+
+        const trends = await getGoogleTrends()
+        const choice = readline.keyInSelect(trends, 'Choose your trend: ')
+
+        return trends[choice]
+    }
+
+    async function getGoogleTrends() {
+        const parser = new Parser()
+        const trends = await parser.parseURL(TREND_URL)
+        return trends.items.map(({title}) => title)
     }
 
     function askAndReturnPrefix() {
